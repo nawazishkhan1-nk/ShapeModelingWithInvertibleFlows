126c6d0d4e4e2cc343dc76108760633ac13c9cf8
nawazishkhan1-nk <nawazishkhan1.nk@gmail.com>
2023-03-06 19:50:38 -0700

    manifold flow clean and distributions jit compatible

--- experiments/scripts-final/pancreas.sh
+++ experiments/scripts-final/pancreas.sh
@@ -6,14 +6,17 @@ cd "/home/sci/nawazish.khan/ShapeModelingWithInvertibleFlows/experiments/"
 scripts_dir="scripts-final"
 
 
-expt_num=3
-dataset="pancreas"
+expt_num=1
+dataset="pancreas-test-jit"
 
 
 config_path="$expt_num-$dataset.config"
 
 # train and evaluate
-# python -u train.py -c $scripts_dir/$config_path
+python -u train.py -c $scripts_dir/$config_path
 
 # # evaluate only
-python -u train.py -c $scripts_dir/$config_path --eval_model
+# python -u train.py -c $scripts_dir/$config_path --eval_model
+
+# Serialize Model only
+python -u train.py -c $scripts_dir/$config_path --serialize_model
--- manifold_flow/flows/manifold_flow.py
+++ manifold_flow/flows/manifold_flow.py
@@ -1,15 +1,196 @@
 import torch
 import logging
+import numpy as np
+from torch import nn
 
-from manifold_flow.transforms import ProjectionSplit
+
+from manifold_flow.utils import various
 from manifold_flow.utils.various import product
-from manifold_flow import distributions, transforms
-from manifold_flow.flows import BaseFlow
+# from manifold_flow import distributions, transforms
+from manifold_flow import transforms
+# from manifold_flow.flows import BaseFlow
 
 logger = logging.getLogger(__name__)
 
 
-class ManifoldFlow(BaseFlow):
+# Jit Compatible Distributions
+class StandardNormalModified():
+    """A multivariate Normal with zero mean and unit covariance."""
+
+    def __init__(self, shape):
+        super().__init__()
+        self._shape = torch.Size(shape)
+        self._log_z = 0.5 * np.prod(shape) * np.log(2 * np.pi)
+
+    def _log_prob(self, inputs, context):
+        # Note: the context is ignored.
+        if inputs.shape[1:] != self._shape:
+            raise ValueError("Expected input of shape {}, got {}".format(self._shape, inputs.shape[1:]))
+        neg_energy = -0.5 * various.sum_except_batch(inputs ** 2, num_batch_dims=1)
+        return neg_energy - self._log_z
+
+    def _sample(self, num_samples, context):
+        if context is None:
+            return torch.randn(num_samples, *self._shape)
+        else:
+            # The value of the context is ignored, only its size is taken into account.
+            context_size = context.shape[0]
+            samples = torch.randn(context_size * num_samples, *self._shape)
+            return various.split_leading_dim(samples, [context_size, num_samples])
+
+
+    def sample(self, num_samples, context=None, batch_size=None):
+        """Generates samples from the distribution. Samples can be generated in batches.
+
+        Args:
+            num_samples: int, number of samples to generate.
+            context: Tensor or None, conditioning variables. If None, the context is ignored.
+            batch_size: int or None, number of samples per batch. If None, all samples are generated
+                in one batch.
+
+        Returns:
+            A Tensor containing the samples, with shape [num_samples, ...] if context is None, or
+            [context_size, num_samples, ...] if context is given.
+        """
+        if not various.is_positive_int(num_samples):
+            raise TypeError("Number of samples must be a positive integer.")
+
+        if context is not None:
+            context = torch.as_tensor(context)
+
+        if batch_size is None:
+            return self._sample(num_samples, context)
+
+        else:
+            if not various.is_positive_int(batch_size):
+                raise TypeError("Batch size must be a positive integer.")
+
+            num_batches = num_samples // batch_size
+            num_leftover = num_samples % batch_size
+            samples = [self._sample(batch_size, context) for _ in range(num_batches)]
+            if num_leftover > 0:
+                samples.append(self._sample(num_leftover, context))
+            return torch.cat(samples, dim=0)
+
+    def _mean(self, context):
+        if context is None:
+            return torch.zeros(self._shape)
+        else:
+            # The value of the context is ignored, only its size is taken into account.
+            return torch.zeros(context.shape[0], *self._shape)
+
+class RescaledNormalModified():
+    """A multivariate Normal with zero mean and a diagonal covariance that is epsilon^2 along each diagonal entry of the matrix."""
+
+    def __init__(self, shape, std=1.0, clip=10.0):
+        super().__init__()
+        self._shape = torch.Size(shape)
+        self.std = std
+        self._clip = clip
+        self._log_z = 0.5 * np.prod(shape) * np.log(2 * np.pi) + np.prod(shape) * np.log(self.std)
+
+    def _log_prob(self, inputs, context):
+        # Note: the context is ignored.
+        if inputs.shape[1:] != self._shape:
+            raise ValueError("Expected input of shape {}, got {}".format(self._shape, inputs.shape[1:]))
+        if self._clip is not None:
+            inputs = torch.clamp(inputs, -self._clip, self._clip)
+        neg_energy = -0.5 * various.sum_except_batch(inputs ** 2, num_batch_dims=1) / self.std ** 2
+        return neg_energy - self._log_z
+
+    def _sample(self, num_samples, context):
+        if context is None:
+            return self.std * torch.randn(num_samples, *self._shape)
+        else:
+            # The value of the context is ignored, only its size is taken into account.
+            context_size = context.shape[0]
+            samples = self.std * torch.randn(context_size * num_samples, *self._shape)
+            return various.split_leading_dim(samples, [context_size, num_samples])
+
+    def _mean(self, context):
+        if context is None:
+            return torch.zeros(self._shape)
+        else:
+            # The value of the context is ignored, only its size is taken into account.
+            return torch.zeros(context.shape[0], *self._shape)
+
+    def sample(self, num_samples, context=None, batch_size=None):
+        """Generates samples from the distribution. Samples can be generated in batches.
+
+        Args:
+            num_samples: int, number of samples to generate.
+            context: Tensor or None, conditioning variables. If None, the context is ignored.
+            batch_size: int or None, number of samples per batch. If None, all samples are generated
+                in one batch.
+
+        Returns:
+            A Tensor containing the samples, with shape [num_samples, ...] if context is None, or
+            [context_size, num_samples, ...] if context is given.
+        """
+        if not various.is_positive_int(num_samples):
+            raise TypeError("Number of samples must be a positive integer.")
+
+        if context is not None:
+            context = torch.as_tensor(context)
+
+        if batch_size is None:
+            return self._sample(num_samples, context)
+
+        else:
+            if not various.is_positive_int(batch_size):
+                raise TypeError("Batch size must be a positive integer.")
+
+            num_batches = num_samples // batch_size
+            num_leftover = num_samples % batch_size
+            samples = [self._sample(batch_size, context) for _ in range(num_batches)]
+            if num_leftover > 0:
+                samples.append(self._sample(num_leftover, context))
+            return torch.cat(samples, dim=0)
+
+
+# Jit Compatible Projections
+class ProjectionSplit(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.input_dim_total = product(input_dim)
+        self.output_dim_total = product(output_dim)
+        self.mode_in = "vector" if isinstance(input_dim, int) else "image"
+        self.mode_out = "vector" if isinstance(input_dim, int) else "image"
+
+        logger.debug("Set up projection from %s with dimension %s to %s with dimension %s", self.mode_in, self.input_dim, self.mode_out, self.output_dim)
+
+        assert self.input_dim_total >= self.output_dim_total, "Input dimension has to be larger than output dimension"
+
+    def forward(self, inputs):
+        if self.mode_in == "vector" and self.mode_out == "vector":
+            u = inputs[:, : self.output_dim]
+            rest = inputs[:, self.output_dim :]
+        elif self.mode_in == "image" and self.mode_out == "vector":
+            h = inputs.view(inputs.size(0), -1)
+            u = h[:, : self.output_dim]
+            rest = h[:, self.output_dim :]
+        else:
+            raise NotImplementedError("Unsuppoorted projection modes {}, {}".format(self.mode_in, self.mode_out))
+        return u, rest
+
+    def inverse(self, inputs, orthogonal_inputs=None):
+        if orthogonal_inputs is None:
+            orthogonal_inputs = torch.zeros(inputs.size(0), self.input_dim_total - self.output_dim)
+        if self.mode_in == "vector" and self.mode_out == "vector":
+            x = torch.cat((inputs, orthogonal_inputs), dim=1)
+        elif self.mode_in == "image" and self.mode_out == "vector":
+            c, h, w = self.input_dim
+            x = torch.cat((inputs, orthogonal_inputs), dim=1)
+            x = x.view(inputs.size(0), c, h, w)
+        else:
+            raise NotImplementedError("Unsuppoorted projection modes {}, {}".format(self.mode_in, self.mode_out))
+        return x
+
+
+class ManifoldFlow(nn.Module):
     """ Manifold-based flow (base class for FOM, M-flow, PIE) """
 
     def __init__(self, data_dim, latent_dim, outer_transform, inner_transform=None, pie_epsilon=1.0e-2, apply_context_to_outer=True, clip_pie=False):
@@ -23,8 +204,8 @@ class ManifoldFlow(BaseFlow):
 
         assert self.total_latent_dim < self.total_data_dim
 
-        self.manifold_latent_distribution = distributions.StandardNormal((self.total_latent_dim,))
-        self.orthogonal_latent_distribution = distributions.RescaledNormal(
+        self.manifold_latent_distribution = StandardNormalModified((self.total_latent_dim,))
+        self.orthogonal_latent_distribution = RescaledNormalModified(
             (self.total_data_dim - self.total_latent_dim,), std=pie_epsilon, clip=None if not clip_pie else clip_pie * pie_epsilon
         )
         self.projection = ProjectionSplit(self.total_data_dim, self.total_latent_dim)
@@ -158,7 +339,12 @@ class ManifoldFlow(BaseFlow):
 
     def _report_model_parameters(self):
         """ Reports the model size """
-        super()._report_model_parameters()
+
+        all_params = sum(p.numel() for p in self.parameters())
+        trainable_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        size = all_params * (32 / 8)  # Bytes
+        logger.info("Model has %.1f M parameters (%.1f M trainable) with an estimated size of %.1f MB", all_params / 1e6, trainable_params / 1.0e6, size / 1.0e6)
+
         inner_params = sum(p.numel() for p in self.inner_transform.parameters())
         outer_params = sum(p.numel() for p in self.outer_transform.parameters())
         logger.info("  Outer transform: %.1f M parameters", outer_params / 1.0e06)
